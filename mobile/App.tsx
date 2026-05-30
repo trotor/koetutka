b4f511ec9a56f5cd8bc08180db346feacfe3b20e
Tero <tero.ronkko@gmail.com>
2026-05-31 00:06:19 +0300

    Initialize store from AsyncStorage on app startup

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'react-native';
+import { useStore } from './src/lib/store';
 import RootNavigator from './src/navigation';
 
 export default function App() {
+  const init = useStore((s) => s.initFromStorage);
+
+  useEffect(() => {
+    void init();
+  }, [init]);
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
